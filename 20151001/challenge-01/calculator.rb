b13356cbd37451c93af00dbc401b814fe4ae86fc
Adriano Chambel M. Lima <acmlima.softweb@gmail.com>
2015-10-01 21:28:45 -0300

    method add done

--- 20151001/challenge-01/calculator.rb
+++ 20151001/challenge-01/calculator.rb
@@ -1,5 +1,6 @@
 class Calculator
 	def add(n1, n2)
-		n1 + n2
+		raise TypeError if (n1.to_s =~ /^\d+$/).nil? or (n2.to_s =~ /^\d+$/).nil?
+		n1.to_i + n2.to_i
 	end
 end
--- 20151001/challenge-01/calculator_test.rb
+++ 20151001/challenge-01/calculator_test.rb
@@ -10,8 +10,23 @@ class TestCalculator < Minitest::Test
 		assert_equal(4, @calculator.add(2, 2), "2+2=4")
 	end
 
-	def test_has_add
-		assert_respond_to(@calculator, :add, "should have the add method")
+	def test_add_with_one_string
+		assert_equal(4, @calculator.add("2", 2), "'2'+2=4")
 	end
+
+	def test_add_two_string
+		assert_equal(4, @calculator.add("2", "2"), "'2'+'2'=4")
+	end
+
+	def test_add_with_letters
+		assert_raises TypeError do
+			@calculator.add("a", "b")
+		end
+	end
+
+
+	#def test_has_add
+	#	assert_respond_to(@calculator, :add, "should have the add method")
+	#end
 	
 end
