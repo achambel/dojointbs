3484da7082b080bfe93be4e3b9403e2a1abb97b1
Adriano Chambel M. Lima <acmlima.softweb@gmail.com>
2015-10-08 19:27:10 -0300

    refactor Calculator class

--- 20151001/challenge-01/calculator.rb
+++ 20151001/challenge-01/calculator.rb
@@ -1,17 +1,17 @@
 class Calculator
 	def add(n1, n2)
-			raise ArgumentError if !(numeric?(n1) || numeric?(n2)) 
+			raise ArgumentError if not_a_number?(n1, n2)
 			Float(n1) + Float(n2)
 	end
 
 	def sub(n1, n2)
-		raise ArgumentError if !(numeric?(n1) || numeric?(n2)) 
+		raise ArgumentError if not_a_number?(n1, n2)
 			Float(n1) - Float(n2)
 	end
 	
 private
-	def numeric?(number)
-		Float(number) != nil rescue false
+	def not_a_number?(n1, n2)
+		Float(n1).nil? || Float(n2).nil? rescue true
 	end
 
 end
